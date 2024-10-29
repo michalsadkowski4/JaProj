1bcc54134bcfff26e12ee94f941a169befbddccd
micha <micha@MICHA_PC>
2024-10-29 18:50:24 +0100

    Dodaj pliki projektów.

---
+++ JaProj/JaProj.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <windows.h>
+
+extern "C" int MyProc1(int a, int b);
+
+
+int main()
+{
+    int x = 4;
+    int y = 5;
+    typedef int (*MyProc1Func)(int, int);
+    HINSTANCE hDLL = LoadLibrary(L"JAAsm.dll");
+    MyProc1Func MyProc1 = (MyProc1Func)GetProcAddress(hDLL, "MyProc1");
+    int result = MyProc1(x, y);
+    std::cout << result;
+}
+
