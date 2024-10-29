3ad0f01571608681691bfeec4187cdb2257b5e0b
michalsadkowski4 <michalsadkowski@hotmail.com>
2024-10-29 20:14:39 +0100

    0.12 z main do headera

--- JaProj/JaProj.cpp
+++ JaProj/JaProj.cpp
@@ -2,16 +2,16 @@
 #include <windows.h>
 
 //extern "C" int MyProc1(int a, int b);
-
+typedef int (*MyProc1Func)(int, int);
+HINSTANCE hDLL = LoadLibrary(L"JAAsm.dll");
+MyProc1Func MyProc1 = (MyProc1Func)GetProcAddress(hDLL, "MyProc1");
 
 int main()
 {
     int x = 4;
     int y = 5;
-    typedef int (*MyProc1Func)(int, int);
-    HINSTANCE hDLL = LoadLibrary(L"JAAsm.dll");
-    MyProc1Func MyProc1 = (MyProc1Func)GetProcAddress(hDLL, "MyProc1");
     int result = MyProc1(x, y);
+    
     std::cout << result;
     FreeLibrary(hDLL);
 }
